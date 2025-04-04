0b2f01d8f4533b4a9c2945ffbfc8d7b5bcb0cc10
DheyvisonSz <soaresdhayegue@gmail.com>
2025-04-04 11:00:30 -0300

    Add files via upload

---
+++ CADASTRO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author Aluno
+ */
+public class CADASTRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Informaçôes do estudante
+        String estudanteNome = "Dheyvison Dhayegue";
+        
+        int estudanteID = 7;
+        
+        int estudanteIdade = 15;
+        
+        float estudanteNota = 100.00f;
+                
+        String estudanteSala = "1DsSeduc";
+        
+        //Variavél
+        
+        System.out.println("Nome do Estudante: "  + estudanteNome );
+         System.out.println("ID do Estudante: "  + estudanteID );
+          System.out.println("Idade do Estudante: "  + estudanteIdade  );
+           System.out.println("Nota do Estudante: "  + estudanteNota);
+            System.out.println("Sala do Estudante: "  + estudanteSala );
+            
+        
+        
+    }
+    
+}
